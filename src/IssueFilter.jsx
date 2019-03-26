ca93af941221c09e3bfdd598d192c100cb7633fc
Peter-Li1233 <minxiangli08@gmail.com>
2019-03-26 09:14:16 -0400

    ok

--- src/IssueFilter.jsx
+++ src/IssueFilter.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 // eslint-disable-next-line react/prefer-stateless-function
 export default class IssueFilter extends React.Component {
   render() {
+    // eslint-disable-next-line no-console
+    console.log(this.props);
     return (
       <div>This is a place holder for the issue Filter.</div>
     );
